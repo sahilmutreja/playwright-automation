25ed1f00fc0fb621444fa39681e283e40caea1bb
Sahil Mutreja <sahilmutreja@outlook.com>
2024-12-08 00:59:34 +0100

    Added login test

--- tests/first-test.spec.js
+++ tests/first-test.spec.js
@@ -4,5 +4,22 @@ test('open google page', async ({ page }) => {
     await page.goto('https://www.google.com/')
     const title = await page.title()
     console.log(title)
-    await expect(page).toHaveTitle(title)   
+    await expect(page).toHaveTitle('Google')
 });
+
+test('open practice automation website', async ({page}) => {
+    await page.goto('https://practicetestautomation.com/')
+    // click practice tab
+    await page
+        .locator('li#menu-item-20>a')
+        .click()
+    // click login
+    await page
+        .locator('.wp-block-columns:nth-child(2) p:first-child>a')
+        .click()
+    await page.locator('#username').fill('student')
+    await page.locator('#password').fill('Password123')
+    await page.locator('#submit').click()
+    await expect(page.getByText('Logged In Successfully')).toBeVisible()
+    console.log('test execution success')
+})
