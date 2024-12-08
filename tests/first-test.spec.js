31fb72bce359f246fac12cffe8f36c278a576ce5
Sahil Mutreja <sahilmutreja@outlook.com>
2024-12-09 00:22:49 +0100

    first test updated

--- tests/first-test.spec.js
+++ tests/first-test.spec.js
@@ -1,25 +1,30 @@
 import { test, expect } from '@playwright/test';
 
-test('open google page', async ({ page }) => {
-    await page.goto('https://www.google.com/')
-    const title = await page.title()
-    console.log(title)
-    await expect(page).toHaveTitle('Google')
+test('open google page', async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://www.google.com/');
+    const title = await page.title();
+    console.log(title);
+    await expect(page).toHaveTitle('Google');
 });
 
 test('open practice automation website', async ({ page }) => {
-    await page.goto('https://practicetestautomation.com/')
+    await page.goto('https://practicetestautomation.com/');
     // click practice tab
+    let txtUsername = page.locator('#username');
     await page
         .locator('li#menu-item-20>a')
-        .click()
+        .click();
     // click login
     await page
         .locator('.wp-block-columns:nth-child(2) p:first-child>a')
-        .click()
-    await page.locator('#username').fill('student')
-    await page.locator('#password').fill('Password123')
-    await page.locator('#submit').click()
-    await expect(page.getByText('Logged In Successfully')).toBeVisible()
-    console.log('test execution success')
-})
+        .click();
+    await txtUsername.fill('sahil');
+    await txtUsername.clear();
+    await txtUsername.fill('student');
+    await page.locator('#password').fill('Password123');
+    await page.locator('#submit').click();
+    await expect(page.getByText('Logged In Successfully')).toBeVisible();
+    console.log('test execution success');
+});
