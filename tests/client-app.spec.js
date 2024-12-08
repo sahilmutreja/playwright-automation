4558d0890f93690f79d99b25f66a2d74291e41ab
Sahil Mutreja <sahilmutreja@outlook.com>
2024-12-09 00:23:21 +0100

    New test added with waits

---
+++ tests/client-app.spec.js
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+
+test('Test login and print all product names', async ({ page }) => {
+    await page.goto('https://rahulshettyacademy.com/client/');
+    await page.locator('#userEmail').fill('Woyoba4364@iminko.com');
+    await page.locator('#userPassword').fill('Woyoba4364@iminko.com');
+    await page.locator('#login').click();
+    
+    // Three ways to wait for all the products to be fully loaded
+    
+    // 1. Wait until the first product content is retrieved 
+    // because playwright provides an auto wait for textContent() method
+    // await page.locator('.card-body b').nth(1).textContent()
+    
+    // 2. Wait until network calls have been loaded
+    // await page.waitForLoadState('networkidle'); 
+    
+    // 3. Wait for the specific locator 
+    const products = page.locator('.card-body b');
+    await products.first().waitFor('visible', 7000); // we can also pass any value - "attached" | "detached" | "visible" | "hidden"
+    
+    console.log(await products.allTextContents());
+});
