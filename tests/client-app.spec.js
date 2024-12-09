45f78313799061b5b418799060b605285c9636ad
Sahil Mutreja <sahilmutreja@outlook.com>
2024-12-09 17:46:19 +0100

    login page controls test

--- tests/client-app.spec.js
+++ tests/client-app.spec.js
@@ -1,5 +1,19 @@
 import { test, expect } from '@playwright/test';
 
+
+test.only('login page controls test', async ({page})=>{
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    await page.locator('#username').fill('test-user');
+    await page.locator('#password').fill('test-password');
+    await page.locator('.radiotextsty').nth(1).click(); // Click User radio button
+    await page.locator('#okayBtn').click(); // click okay in prompt 
+    const dropdown = page.locator("[data-style='btn-info']")
+    await dropdown.selectOption('teach');
+    await page.locator('#terms').check()
+    await expect(page.locator("[href*='documents-request']"))
+            .toHaveAttribute('class','blinkingTexts');
+});
+
 test('Test login and print all product names', async ({ page }) => {
     await page.goto('https://rahulshettyacademy.com/client/');
     await page.locator('#userEmail').fill('Woyoba4364@iminko.com');
