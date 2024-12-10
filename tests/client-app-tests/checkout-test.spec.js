a004353352c1ef1413679fd34e2f86c4f210a214
Sahil Mutreja <sahilmutreja@outlook.com>
2024-12-11 00:49:07 +0100

    checkout test completed

--- tests/client-app-tests/checkout-test.spec.js
+++ tests/client-app-tests/checkout-test.spec.js
@@ -1,15 +1,17 @@
 import { test, expect } from '@playwright/test';
+import exp from 'constants';
 
 
-test('Print all product names test', async ({ page }) => {
+test('checkout product test', async ({ page }) => {
+    const email = 'Woyoba4364@iminko.com';
     // Goto page
     await page.goto('https://rahulshettyacademy.com/client/');
 
     // Get required locators
-    await page.locator('#userEmail').fill('Woyoba4364@iminko.com');
-    await page.locator('#userPassword').fill('Woyoba4364@iminko.com');
+    await page.locator('#userEmail').fill(email);
+    await page.locator('#userPassword').fill(email);
     await page.locator('#login').click();
-    
+
     // Wait for first product
     await page.locator('.card-body').first().waitFor();
 
@@ -19,7 +21,37 @@ test('Print all product names test', async ({ page }) => {
         const product = allProducts.nth(index);
         if (await product.locator('h5>b').textContent() == 'IPHONE 13 PRO')
             await product.locator('button').last().click();
-    } 
-    await page.pause();
-    // Click on Add to Cart 
+    }
+    // Go to cart and verify if product is added and checkout
+    await page.locator("[routerlink*='cart']").click()
+    await expect(page.locator('h3', { hasText: 'IPHONE 13 PRO' })).toBeVisible();
+    await page.locator('text=Checkout').click();
+
+    // Enter India as country and choose correct option from the dynamic options
+    await page.locator('[placeholder="Select Country"]').pressSequentially('Ind');
+    // await page.getByText('India', { exact: true }).waitFor();
+    // await page.getByRole('India', { exact: true }).click();
+    const dropdown = page.locator('.form-group .ta-results');
+    await dropdown.waitFor()
+    dropdown.getByText('India', { exact: true }).click();
+
+    // Verify if logged in email is shown on checkout
+    await expect(page.locator('.user__name>label')).toHaveText(email);
+
+    // Click checkout verify success message and save order id
+    await page.locator('.action__submit').click();
+    await expect(page.locator('.hero-primary'))
+        .toHaveText(' Thankyou for the order. ');
+    const orderId = await page.locator("label[class='ng-star-inserted']").textContent();
+    
+    // Go to order history page and check if order id is present 
+    await page.locator('label[routerlink*="myorders"]').click();
+    const rowWithOrderId = page.getByRole('row', { name: '6758cba6e2b5443b1fee67b8' });
+    await expect(rowWithOrderId).toBeVisible();
+
+    // Click view button in the row for our order id
+    await rowWithOrderId.locator('.btn-primary').click();
+    
+    // Asser if our product is present on order summary page
+    await expect(page.locator('[class="title"]')).toHaveText('IPHONE 13 PRO');
 });
