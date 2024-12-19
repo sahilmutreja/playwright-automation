5fcfb7f1e1ee96fc084fabf09dd596cac381b488
Sahil Mutreja <sahilmutreja@outlook.com>
2024-12-19 15:11:20 +0100

    getBy locators

---
+++ tests/getby-locators-test.spec.js
@@ -0,0 +1,24 @@
+import { expect, test } from '@playwright/test';
+test('using getBy locators', async ({ page }) => {
+    await page.goto('https://rahulshettyacademy.com/angularpractice/');
+    // getByLabel
+    await page.getByLabel('Check me out if you Love IceCreams!').check();
+    await page.getByLabel('Student').check();
+    await page.getByLabel('Gender').selectOption('Female');
+    // getByPlaceholder
+    await page.getByPlaceholder('Password').fill('dummypassword');
+    // getByRole
+    await page.getByRole('button', { name: 'Submit' }).click();
+    // getByText
+    await expect(page.getByText('Success! The Form has been submitted successfully!.'))
+        .toBeVisible();
+    // 
+    await page.getByRole('link', { name: 'Shop' }).click();
+    await page.locator('app-card')
+        .filter({ hasText: 'Nokia Edge' })
+        .getByRole('button')
+        .click();
+    await page.locator('.nav-link')
+        .filter({ hasText: 'Checkout' })
+        .click();
+});
