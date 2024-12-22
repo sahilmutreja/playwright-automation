b40b38ccbf28a0549c9f048818f515a2fb71272b
Sahil Mutreja <sahilmutreja@outlook.com>
2024-12-22 14:36:18 +0100

    Running tests in parallel

--- playwright.config.js
+++ playwright.config.js
@@ -14,13 +14,13 @@ module.exports = defineConfig({
     timeout: 5 * 1000
   },
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 5,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
